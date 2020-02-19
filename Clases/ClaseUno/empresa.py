a19d980e4b5b7313e26bb7cfa7b7c947d6d0fc78
Daniel <zarate.daniel@uces.edu.co>
2020-02-19 12:11:33 -0500

    holamundo

---
+++ Clases/ClaseUno/empresa.py
@@ -0,0 +1,4 @@
+nombre = "Italmotos"
+NumeroEmpleados = 23
+EdadEmpresa= .8
+Salario = 2500000
---
+++ Clases/ClaseUno/holamundo.py
@@ -0,0 +1,10 @@
+#Codigo primera clase 
+nombre = "Daniel"
+edad = 19
+dinero = 100.3
+print(nombre)
+print(type(nombre))
+print(edad)
+print(type(edad))
+print(dinero)
+print(type(dinero))
