580a052f143f66164cfc54f107968625ad7bbce2
Daniel <zarate.daniel@uces.edu.co>
2020-02-19 13:31:56 -0500

    Clase2

--- Clases/ClaseUno/empresa.py
+++ Clases/ClaseUno/empresa.py
@@ -1,4 +1,10 @@
-nombre = "Italmotos"
-NumeroEmpleados = 23
-EdadEmpresa= .8
-Salario = 2500000
+NOMBRE_EMPRESA = "Italmotos"
+numeroEmpleados = 23
+edadEmpresa= .8
+salario = 2500000
+print("El nombre de la empresa es:", NOMBRE_EMPRESA)
+print(type(NOMBRE_EMPRESA))
+print(edadEmpresa)
+print(type(edadEmpresa))
+print(salario)
+print(type(salario))
