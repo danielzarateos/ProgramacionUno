aadecc5f7a61b562ea49507239f0764b0bfd555a
Daniel <zarate.daniel@uces.edu.co>
2020-02-21 12:21:35 -0500

    Buenos Dias

--- Clases/ClaseUno/empresa.py
+++ Clases/ClaseUno/empresa.py
@@ -1,10 +1,15 @@
 NOMBRE_EMPRESA = "Italmotos"
+MENSAJE_CANTIDAD_EMPLEADOS = "La cantidad de empleados en la empresa es de :"
+MENSAJE_EDAD_EMPRESA = "Años de la empresa:"
+MENSAJE_SALARIO = "El salario base de la empresa es de:"
 numeroEmpleados = 23
 edadEmpresa= .8
 salario = 2500000
 print("El nombre de la empresa es:", NOMBRE_EMPRESA)
 print(type(NOMBRE_EMPRESA))
-print(edadEmpresa)
+print(MENSAJE_CANTIDAD_EMPLEADOS, numeroEmpleados)
+print(type(numeroEmpleados))
+print(MENSAJE_EDAD_EMPRESA,edadEmpresa)
 print(type(edadEmpresa))
-print(salario)
+print(MENSAJE_SALARIO,salario)
 print(type(salario))
