55af63b88e0bdcf25b5c9b41a6342eb1ccf2be1a
Daniel <zarate.daniel@uces.edu.co>
2020-03-13 10:59:56 -0500

    tarea

---
+++ Clases/ClaseSeis/tareaFunciones.py
@@ -0,0 +1,17 @@
+def sumar(x,y) : 
+    suma = x + y 
+    return suma
+def restar(x,y) :
+    resta = x - y
+    return resta
+def multiplicar(x,y) :
+    multiplicacion = x * y
+    return multiplicacion
+def dividir(x,y) :
+    division = x / y
+    return division
+
+print(sumar(2,2))
+print(restar(2,23))
+print(multiplicar(3,7))
+print(dividir(25,5))
